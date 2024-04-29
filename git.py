6484f794642b3d079e05c4b9a71d431fc134f512
itsmasad <Muhammadasadofficial@gmail.com>
2024-04-29 19:57:56 -0400

    test

--- git.py
+++
@@ -1,5 +0,0 @@
-def solution(n):
-    print(n)
-
-string = "This is a demo code"
-solution(string)
---
+++ test_git.py
@@ -0,0 +1,9 @@
+import pytest
+
+def test_solution():
+    str_value = "This is a demo code"
+    n_value = 1
+    assert n_value == 1
+    # Add more assertions or tests as needed
+
+
